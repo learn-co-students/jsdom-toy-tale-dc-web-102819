f0f0f46711efe6ac844946e2e20a0128b8ebcf96
github username <github email address>
2019-12-13 12:13:11 -0500

    working like

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ let addToy = false
 document.addEventListener("DOMContentLoaded", ()=>{
   const addBtn = document.querySelector('#new-toy-btn')
   const toyForm = document.querySelector('.container')
+  getToyForm().addEventListener('submit', createToy)
   getAllToys()
   addBtn.addEventListener('click', () => {
     // hide & seek with the form
@@ -13,7 +14,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
       toyForm.style.display = 'none'
     }
   })
-
+  
 })
 
 
@@ -26,31 +27,65 @@ function getAllToys() {
   } )
 }
 
+// pokemonCard.id = `pokemon-${pokemon.id}`
 function RenderToys(toy) {
   // debugger
   let toyContainer = document.getElementById("toy-collection") 
-
   let newToy = document.createElement("div")
   newToy.classList.add("card") 
   toyContainer.appendChild(newToy)  
-
+  
   let toyNameLabel = document.createElement("h2") 
   toyNameLabel.innerText = toy.name
   let displayImage = document.createElement("img")
   displayImage.src = toy.image
-  
-  
+  displayImage.classList.add("toy-avatar") 
+  let toyLikesLabel = document.createElement("p")
+  toyLikesLabel.innerText = `${toy.likes} Likes`
+  toyLikesLabel.id = `likelabel-${toy.id}`
+  let toyLikeBtn = document.createElement("button")
+  toyLikeBtn.classList.add("likeBtn")
+  toyLikeBtn.id = `like-${toy.id}`
+  toyLikeBtn.innerText = "Like <3"
+
+  newToy.addEventListener("click", increaseLikes)
+
   
   newToy.appendChild(toyNameLabel)  
   newToy.appendChild(displayImage)
+  newToy.appendChild(toyLikesLabel)
+  newToy.appendChild(toyLikeBtn) 
+  
+  
 }
 
+function increaseLikes(e){
+  
+  
+}
 
+function getToyForm(){
+  return document.getElementById("new-toy")
+}
 
-{/* <div class="card">
-<h2>Woody</h2>
-<img src=toy_image_url class="toy-avatar" />
-<p>4 Likes </p>
-<button class="like-btn">Like <3</button>
-</div>
-  */}
+function createToy(e){
+  e.preventDefault()
+  let newToy = {"name": document.getElementById("name-input").value,
+  "image": document.getElementById("url-input").value,
+  "likes": 0
+}
+
+json_string = JSON.stringify(newToy)
+fetch("http://localhost:3000/toys", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: json_string
+})
+.then(res => res.json())
+.then(data => RenderToys(data))
+
+  getToyForm().reset()
+}
+
