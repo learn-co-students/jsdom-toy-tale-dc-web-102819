e0f280124c501fcbe7cca24aa694a1c5b4d4d9e8
github username <github email address>
2019-12-13 10:14:10 -0500

    initial changes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ let addToy = false
 document.addEventListener("DOMContentLoaded", ()=>{
   const addBtn = document.querySelector('#new-toy-btn')
   const toyForm = document.querySelector('.container')
+  getAllToys()
   addBtn.addEventListener('click', () => {
     // hide & seek with the form
     addToy = !addToy
@@ -14,3 +15,42 @@ document.addEventListener("DOMContentLoaded", ()=>{
   })
 
 })
+
+
+function getAllToys() {
+  fetch("http://localhost:3000/toys")
+  .then(response => response.json())
+  // .then(getAllToys => console.log(getAllToys)) 
+  .then(getAllToys => {
+    getAllToys.forEach(toy => RenderToys(toy))
+  } )
+}
+
+function RenderToys(toy) {
+  // debugger
+  let toyContainer = document.getElementById("toy-collection") 
+
+  let newToy = document.createElement("div")
+  newToy.classList.add("card") 
+  toyContainer.appendChild(newToy)  
+
+  let toyNameLabel = document.createElement("h2") 
+  toyNameLabel.innerText = toy.name
+  let displayImage = document.createElement("img")
+  displayImage.src = toy.image
+  
+  
+  
+  newToy.appendChild(toyNameLabel)  
+  newToy.appendChild(displayImage)
+}
+
+
+
+{/* <div class="card">
+<h2>Woody</h2>
+<img src=toy_image_url class="toy-avatar" />
+<p>4 Likes </p>
+<button class="like-btn">Like <3</button>
+</div>
+  */}
